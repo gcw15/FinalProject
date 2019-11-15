ef601c547ef4e699b2b97b9853a962f82e50a4fa
Grant Wilson <gcw15@pitt.edu>
2019-11-15 15:09:54 -0500

    ran paired 2 sample t test

--- Source/Final_Analysis.R
+++ Source/Final_Analysis.R
@@ -3,3 +3,6 @@ library(readr)
 Seal_Data <- read_csv("Desktop/Biostat/FinalProject/Data/Seal_Data.csv")
 View(Seal_Data)
 
+#data analysis - 2 sample t-test, paired
+t.test(Seal_Data$O2_Nonfeeding, Seal_Data$O2_Feeding, paired = TRUE)
+
